6faad3d7846b2859d8ac7556a531efaacd829201
liltigercode <tigerlillo746@gmail.com>
2025-01-02 23:28:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,11 +13,17 @@ const validationRegex = [
   { regex: /[^A-Za-z0-9]/ } // special characters
 ];
 
+const firstchoice = '<div id="intro"><button id="new">Create new Wallet</button><button id="exst">Import existing Wallet</button></div>';
+
 const signup = '<p class="label" id="pinlbl">Enter Password</p><input type="password" id="pin" class="pin"/><p class="label" id="pin2lbl">Re Enter Password</p><input type="password" id="pin2"  class="pin"/><input type="hidden" id="session"/><div id="setpin" class="submitter">Set Password</div>';
 
 const signin = '<p class="label">Enter Password</p><input type="password" id="pin" class="pin"/><input type="hidden" id="session"/><div id="setpin" class="submitter">Login</div><p>If you forgot your Password you can recover your entire Account with the Seedphrase</p>';
 
 const pinChecklist = '<p class="label" id="pinlbl">Enter Password</p><input type="password" id="pin" class="pin"/><p class="label" id="pin2lbl">Re Enter Password</p><input type="password" id="pin2"  class="pin"/><input type="hidden" id="session"/><div class="password-checklist"><h3 class="checklist-title label">Password should be</h3><ul class="checklist"><li class="list-item">At least 8 character long</li><li class="list-item">At least 1 number</li><li class="list-item">At least 1 lowercase letter</li><li class="list-item">At least 1 uppercase letter</li><li class="list-item">At least 1 special character</li></ul></div><div id="setpin" class="submitter">Set Password</div>';
+
+const newseed = '<div id="newseedbox"><p id="sp"Your New seedphrase :</p><div id="phrase"><input id="word1" value="yellow" class="seed"/><input id="word2" value="fast" class="seed"/><input id="word3" value="total" class="seed"/><input id="word4" value="rain" class="seed"/><input id="word5" value="card" class="seed"/><input id="word6" value="speed" class="seed"/><input id="word7" value="future" class="seed"/><input id="word8" value="cloud" class="seed"/><input id="word9" value="saddle" class="seed"/><input id="word10" value="widow" class="seed"/><input id="word11" value="couch" class="seed"/><input id="word12" value="speaker" class="seed"/><button id="copy">Copy to Clipboard</button></div></div>';
+
+const importseed = '<div id="seedbox"><p id="sp">Enter your seedphrase :</p><div id="phrase"><input id="word1" placeholder="Word 1" class="seed"/><input id="word2" placeholder="Word 2" class="seed"/><input id="word3" placeholder="Word 3" class="seed"/><input id="word4" placeholder="Word 4" class="seed"/><input id="word5" placeholder="Word 5" class="seed"/><input id="word6" placeholder="Word 6" class="seed"/><input id="word7" placeholder="Word 7" class="seed"/><input id="word8" placeholder="Word 8" class="seed"/><input id="word9" placeholder="Word 9" class="seed"/><input id="word10" placeholder="Word 10" class="seed"/><input id="word11" placeholder="Word 11" class="seed"/><input id="word12" placeholder="Word 12" class="seed"/><button id="import">Import Wallet</button></div></div>';
 
 const changed = (e) => {
     const pin = document.getElementById("pin");
